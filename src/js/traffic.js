a4d290432cd2c4e8e07f9abf6946514c412beef5
GabrieOliveiraMartins <gabrolivmartins@gmail.com>
2025-09-02 16:46:28 -0300

    Update traffic.js

--- src/js/traffic.js
+++ src/js/traffic.js
@@ -1,8 +1,14 @@
 function spwanCar(numPista){
-  const carro = document.createElement("img");
-  carro.src = `./assets/images/TrafficCars/car${carNum}.png`;
-  carro.alt = "TrafficCar"
-  carro.classList.add("carro");
-  const div = document.getElementsByTagName("div");
-  const coluna = document.getElementById(`col-${colNum}`)
+  for(let i = 0; i < numPista -1; i++){
+    
+    numCol = Math.floor(Math.random() * numPista)
+    const coluna = document.getElementById(`col-${colNum}`);
+
+    const carro = document.createElement("img");
+    carro.src = `./assets/images/TrafficCars/car${carNum}.png`;
+    carro.alt = "TrafficCar"
+    carro.classList.add("carro");
+    const div = document.getElementsByTagName("div");
+  }
+  
 }
