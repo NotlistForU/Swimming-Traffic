5a1d26f864213263a5ee3064195663389eac932e
GabrieOliveiraMartins <gabrolivmartins@gmail.com>
2025-09-03 09:01:05 -0300

    commit da felicidade PORR
    👍  :)

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,14 @@
+
 $(document).ready(function(){
+  $(".carro").animate({
+    top: "+=500"
+  }, 1000)
+})
+/*$(document).ready(function(){
   $("#car").animate({
     top: "+=500"
   }, 1000, function(){
     alert("cabo oq era doce")
   })
-})
+})*/
+
--- src/js/traffic.js
+++ src/js/traffic.js
@@ -1,14 +1,19 @@
 function spwanCar(numPista){
-  for(let i = 0; i < numPista -1; i++){
-    
-    numCol = Math.floor(Math.random() * numPista)
-    const coluna = document.getElementById(`col-${colNum}`);
 
+    let numCol = (Math.floor(Math.random() * numPista) + 1);
+    let coluna = document.getElementById(`col-${numCol}`);
+    let numCar = Math.floor(Math.random() * 9) + 1;
     const carro = document.createElement("img");
-    carro.src = `./assets/images/TrafficCars/car${carNum}.png`;
+    carro.src = `src/assets/images/TrafficCars/car${numCar}.png`;
     carro.alt = "TrafficCar"
     carro.classList.add("carro");
+    coluna.appendChild(carro);
     const div = document.getElementsByTagName("div");
-  }
+  /*for(let i = 0; i < numPista -1; i++){
+    
+    
+  }*/
   
 }
+
+spwanCar(5);
