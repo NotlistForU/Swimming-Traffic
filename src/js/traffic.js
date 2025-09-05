57045431b6dee203a08df699998097a9c604b4aa
GabrieOliveiraMartins <gabrolivmartins@gmail.com>
2025-09-04 21:07:42 -0400

    commit

--- src/js/traffic.js
+++ src/js/traffic.js
@@ -1,17 +1,22 @@
-function spwanCar(numPista){
-
-    let numCol = (Math.floor(Math.random() * numPista) + 1);
-    let coluna = document.getElementById(`col-${numCol}`);
-    let numCar = Math.floor(Math.random() * 9) + 1;
-    const carro = document.createElement("img");
-    carro.src = `src/assets/images/TrafficCars/car${numCar}.png`;
-    carro.alt = "TrafficCar"
-    carro.classList.add("carroBaixo");
-    coluna.appendChild(carro);
+function spwanCar(numPista, quantCar){
+    for(let i = 1; i < quantCar; i++){
+      let numY = Math.floor(Math.random()* 6000) + 1;
+      let carroY = numY + "px";
+      let numCol = (Math.floor(Math.random() * numPista) + 1);
+      let coluna = document.getElementById(`col-${numCol}`);
+      let numCar = Math.floor(Math.random() * 9) + 1;
+      const carro = document.createElement("img");
+      carro.style.top = carroY; 
+      carro.src = `src/assets/images/TrafficCars/car${numCar}.png`;
+      carro.alt = "TrafficCar"
+      carro.classList.add("carroBaixo");
+      coluna.appendChild(carro);
+    }
+  
   
 }
 
-spwanCar(5);
+spwanCar(5, 10);
 
 
 
