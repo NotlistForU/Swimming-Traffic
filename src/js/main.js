b0c912428b4653e71bff1902150c02c33f703e60
GabrieOliveiraMartins <gabrolivmartins@gmail.com>
2025-09-26 15:53:28 -0300

    c

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,17 @@
+document.addEventListener('keydown', function (event) {
+  if ((event.ctrlKey || event.metaKey) && 
+      (event.key === '+' || event.key === '-' || event.key === '=')) {
+    event.preventDefault();
+  }
+});
+
+document.addEventListener('wheel', function (event) {
+  if (event.ctrlKey) {
+    event.preventDefault();
+  }
+}, { passive: false });
+
+
 function gameOver() {
   gameStarted = false;
   motorSound.pause();
