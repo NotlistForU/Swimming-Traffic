bb4be5a034f4417cb10a7e2a2a15db1fc9ca9e6f
GabrieOliveiraMartins <gabrolivmartins@gmail.com>
2025-09-30 07:57:41 -0300

    remove pasta de build

--- SwimmingTraffic-win32-x64/resources/app/src/js/road.js
+++
@@ -1,8 +0,0 @@
- let novaVelocidade = 0.2;
-function atualizarVelocidadeFaixa() {
-  // quanto maior a dificuldade, menor a duração da animação
-    novaVelocidade = Math.max(0.05, 0.2 / dificuldade); 
-    document.querySelectorAll(".pista1").forEach(faixa => {
-    faixa.style.setProperty("--faixa-speed", novaVelocidade + "s");
-  });
-}
