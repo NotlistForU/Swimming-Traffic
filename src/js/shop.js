ea22d45be6422f9f4b37463e7988a4ce46143983
GabrieOliveiraMartins <gabrolivmartins@gmail.com>
2025-11-08 15:14:16 -0300

    update price

--- src/js/shop.js
+++ src/js/shop.js
@@ -5,10 +5,10 @@
 
 let carros = [
   { id: 1, nome: "Golf GTI", prefix: "car1", preco: 0, kmNecessario: 0, comprado: true },
-  { id: 2, nome: "BMW M4 cs", prefix: "car2", preco: 60, kmNecessario: 1.3, comprado: false },
-  { id: 3, nome: "Supra MK4", prefix: "car3", preco: 160, kmNecessario: 2.5, comprado: false },
-  { id: 4, nome: "Nissan GT-R Nismo", prefix: "car4", preco: 240, kmNecessario: 4.5, comprado: false },
-  { id: 5, nome: "La Ferrari", prefix: "car5", preco: 500, kmNecessario: 8.5, comprado: false }
+  { id: 2, nome: "BMW M4 cs", prefix: "car2", preco: 30, kmNecessario: 1.3, comprado: false },
+  { id: 3, nome: "Supra MK4", prefix: "car3", preco: 45, kmNecessario: 2.5, comprado: false },
+  { id: 4, nome: "Nissan GT-R Nismo", prefix: "car4", preco: 60, kmNecessario: 4.5, comprado: false },
+  { id: 5, nome: "La Ferrari", prefix: "car5", preco: 70, kmNecessario: 8.5, comprado: false }
 ];
 
 // =====================
